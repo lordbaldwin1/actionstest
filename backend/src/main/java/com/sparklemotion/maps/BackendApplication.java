a9e736b13915cefc6e03dbcd46fced93c3a6c77e
Zachary Springer <zachar7@pdx.edu>
2024-06-04 22:34:03 -0700

    test

--- backend/src/main/java/com/sparklemotion/maps/BackendApplication.java
+++ backend/src/main/java/com/sparklemotion/maps/BackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+//COMMENT TEST HEHEHE
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.sparklemotion.*")
 public class BackendApplication {
